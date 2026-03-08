8bb10b59eb422eb55729cc03ce09ebbfe1e6efd8
Anil-Mhj <maharjananil377@gmail.com>
2026-03-08 13:15:55 +0545

    Implemented dynamic programming

---
+++ Lab_4/matrix_chain_multiplication.c
@@ -0,0 +1,54 @@
+// c program to implement matrix chain multiplication
+#include <stdio.h>
+#include <limits.h>
+
+int MatrixChainOrder(int p[], int n)
+{
+    int m[n][n];
+    int i, j, k, L, q;
+
+    // Cost is zero when multiplying one matrix
+    for (i = 1; i < n; i++)
+        m[i][i] = 0;
+
+    // L is chain length
+    for (L = 2; L < n; L++)
+    {
+        for (i = 1; i < n - L + 1; i++)
+        {
+            j = i + L - 1;
+            m[i][j] = INT_MAX;
+
+            for (k = i; k <= j - 1; k++)
+            {
+                q = m[i][k] + m[k + 1][j] + p[i - 1] * p[k] * p[j];
+
+                if (q < m[i][j])
+                    m[i][j] = q;
+            }
+        }
+    }
+
+    return m[1][n - 1];
+}
+
+int main()
+{
+    int n, i;
+
+    printf("Enter number of matrices: ");
+    scanf("%d", &n);
+
+    int p[n + 1];
+
+    printf("Enter dimensions array (size %d):\n", n + 1);
+    for (i = 0; i <= n; i++)
+    {
+        scanf("%d", &p[i]);
+    }
+
+    printf("Minimum number of multiplications is %d\n",
+           MatrixChainOrder(p, n + 1));
+
+    return 0;
+}
